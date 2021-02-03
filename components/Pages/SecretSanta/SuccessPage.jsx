826b0da8fd20b134df32a8707169fad555897c26
David Naranjo <dnaranjo89@gmail.com>
2021-02-03 23:26:01 +0100

    Add success page

--- components/Pages/SecretSanta/SuccessPage.jsx
+++ components/Pages/SecretSanta/SuccessPage.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import useTranslation from 'next-translate/useTranslation';
+import Trans from 'next-translate/Trans';
+import Typography from '@material-ui/core/Typography';
+import Page from '../../Page/Page.jsx';
+import STYLES from './SuccessPage.module.scss';
 
-const SuccessPage = props => <div>Congratulations!</div>;
+const SuccessPage = () => {
+  const { t } = useTranslation('DrawSecretSanta');
+
+  return (
+    <Page htmlTitle={t('html_title')} pageType="Secret Santa Successfully Created" noIndex>
+      <Typography align="center" variant="h1" className={STYLES.Title}>
+        {t('congratulations')} <br />
+        {t('draw_created')}
+      </Typography>
+
+      <Trans i18nKey="DrawSecretSanta:success_details" components={[<p />]} />
+    </Page>
+  );
+};
 
 SuccessPage.propTypes = {};
 
