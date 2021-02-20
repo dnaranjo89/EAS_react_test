886fa1f16991820a1149a38dcb7c61c2865dc2bb
David Naranjo <dnaranjo89@gmail.com>
2021-02-20 20:08:20 +0100

    Remove unnecessary props

--- components/BoxWithBorder/BoxWithBorder.jsx
+++ components/BoxWithBorder/BoxWithBorder.jsx
@@ -5,11 +5,11 @@ import STYLES from './BoxWithBorder.module.scss';
 
 const c = classnames.bind(STYLES);
 
-const BoxWithBorder = ({ error, children, className, ...rest }) => (
+const BoxWithBorder = ({ error, children, className, 'data-testid': dataTestId }) => (
   <div
     role="presentation"
     className={c(STYLES.container, { [STYLES.error]: error }, className)}
-    {...rest}
+    data-testid={dataTestId}
   >
     {children}
   </div>
@@ -19,10 +19,12 @@ BoxWithBorder.propTypes = {
   className: PropTypes.string,
   error: PropTypes.bool,
   children: PropTypes.node.isRequired,
+  'data-testid': PropTypes.string,
 };
 BoxWithBorder.defaultProps = {
   className: null,
   error: false,
+  'data-testid': null,
 };
 
 export default BoxWithBorder;
