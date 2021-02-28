5461a35ea4b0ce79139c4016fdc741d840938ba4
David Naranjo <dnaranjo89@gmail.com>
2021-02-28 15:14:32 +0100

    Don't break when the read requests fail

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -6,11 +6,12 @@ import { analyticsTypesBySlug } from '../constants/analyticsTypes';
 export const serializeResponse = draw => JSON.parse(JSON.stringify(draw));
 
 const fetchData = async ({ drawId, urlSlug }) => {
-  // getServerSideProps does a strict serialization which fails when serialising Date
-  // By doing stringify & parse we make sure that dates are strings and not Date objects
-  // https://github.com/vercel/next.js/issues/11993#issuecomment-617937409
-  const draw = serializeResponse(await fetchDraw({ urlSlug, drawId }));
   try {
+    // getServerSideProps does a strict serialization which fails when serialising Date
+    // By doing stringify & parse we make sure that dates are strings and not Date objects
+    // https://github.com/vercel/next.js/issues/11993#issuecomment-617937409
+    const response = await fetchDraw({ urlSlug, drawId });
+    const draw = serializeResponse(response);
     return {
       draw,
     };
