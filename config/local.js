985bae5d86d46ca5e2427af179ddb0a401024966
David Naranjo <dnaranjo89@gmail.com>
2021-03-18 20:39:30 +0100

    Disable Mixpanel and Hotjar

--- config/local.js
+++ config/local.js
@@ -3,9 +3,9 @@ const config = {
   googleAnalyticsID: 'UA-62791775-3',
   mixpanelID: '5ad8df79215a2cd8df2c93a126dcaa23',
   mixpanelDebug: true,
-  hotjarEnabled: true,
+  hotjarEnabled: false,
   googleAnalyticsEnabled: true,
-  mixpanelEnabled: true,
+  mixpanelEnabled: false,
   sentryEnabled: true,
   // apiDomain is only used when making calls server side, on the client the calls are relative to the hostname
   apiDomain: 'http://127.0.0.1:8000',
--- config/production.js
+++ config/production.js
@@ -3,9 +3,9 @@ const config = {
   googleAnalyticsID: 'UA-62791775-2',
   mixpanelID: '25bd9d326ae0254f31deec3ae4ce1dd0',
   mixpanelDebug: false,
-  hotjarEnabled: true,
+  hotjarEnabled: false,
   googleAnalyticsEnabled: true,
-  mixpanelEnabled: true,
+  mixpanelEnabled: false,
   sentryEnabled: true,
   // apiDomain is only used when making calls server side, on the client the calls are relative to the hostname
   apiDomain: 'https://echaloasuerte.com',
